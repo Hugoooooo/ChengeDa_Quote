111e7aee7be3cf0a012e9c85ee8dd07402eabad3
Hugo <hugo010911@gmail.com>
2022-11-16 00:09:32 +0800

    起訖時間計算

--- src/components/puch-card/puch-card-edit/puch-card-edit.component.ts
+++ src/components/puch-card/puch-card-edit/puch-card-edit.component.ts
@@ -11,15 +11,21 @@ export class PuchCardEditComponent implements OnInit {
   yearDDL = [];
   month;
   monthDDL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  dataList:Array<CardRecordModel>;
+  dataList: Array<CardRecordModel>;
   constructor() { }
 
   ngOnInit() {
+    let now = new Date();
+    // this.year = now.get
     this.dataList = [];
     for (var i = 1; i <= 30; i++) {
       let year = new Date().getFullYear() - 2;
       this.yearDDL.push(year + i);
     }
+    this.year = now.getFullYear();
+    this.month = now.getMonth() + 1;
+
+    console.log(this.calcTwoTime('0800', '1700'));
   }
 
 
@@ -27,9 +33,9 @@ export class PuchCardEditComponent implements OnInit {
     return new Date(year, month, 0).getDate();
   }
 
-  loadForm(){
+  loadForm() {
     this.dataList = [];
-    let days = this.getDaysInMonth(this.year,this.month);
+    let days = this.getDaysInMonth(this.year, this.month);
     for (var i = 1; i <= days; i++) {
       const addItem = new CardRecordModel();
       addItem.year = this.year;
@@ -37,14 +43,27 @@ export class PuchCardEditComponent implements OnInit {
       addItem.day = i;
       addItem.startTime = '';
       addItem.endTime = '';
-      addItem.dayOff = this.checkWeekend(i);
-      console.log(addItem);
+      addItem.dayOff = this.checkWeekend(new Date(this.year, this.month - 1, i));
+      // console.log(addItem);
       this.dataList.push(addItem);
     }
   }
 
-  checkWeekend(day){
-    return day%6 ==0 || day%7 ==0;
+  checkWeekend(target) {
+    let day = target.getDay();
+    return day == 0 || day == 6;
+  }
+
+  preview() {
+    console.log(this.dataList);
+  }
+
+  calcTwoTime(startTime, endTime) {
+    let start = new Date(`2022/01/01 ${startTime.substr(0, 2)}:${startTime.substr(2, 4)}`);
+    let end = new Date(`2022/01/01 ${endTime.substr(0, 2)}:${endTime.substr(2, 4)}`);
+    let secs = end.getTime() - start.getTime();
+    let hours = secs / 1000 / 60 / 60;
+    return Math.floor(hours);
   }
 
 }
